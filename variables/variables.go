2f6b19774e1465a30b5d38168d57bc88cbc6a711
Akshat <iamakku0.0.0.1@gmail.com>
2025-11-09 21:15:09 +0530

    variables

---
+++ variables/variables.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	//declaring variables
+	var name = "akshat"
+
+	//short hand
+	naam := "akshat"
+
+	var yo int
+
+	yo = 1
+
+	print(yo)
+
+	fmt.Print(name)
+	fmt.Print(naam)
+}
