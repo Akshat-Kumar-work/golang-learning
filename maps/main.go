1fbb9c89a634f231bd9a791bc4d879f515951f5f
Akshat <iamakku0.0.0.1@gmail.com>
2025-11-10 12:50:05 +0530

    feat : maps

---
+++ maps/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	// map is an data structure to store key value pair
+	//note : if key doesnot exist in map it return zero value
+
+	//creating map
+	mp := map[string]int{"ok": 3}
+	fmt.Println(mp)
+
+	//creatig map using make keyword
+	m := make(map[string]string)
+
+	//to set
+	m["k"] = "nice"
+	m["age"] = "40"
+
+	//to get value via key
+	fmt.Println(m["k"])
+
+	delete(m, "k")
+
+	val, exist := m["age"]
+	fmt.Println(val)
+
+	fmt.Println(exist)
+
+	fmt.Print(m)
+}
