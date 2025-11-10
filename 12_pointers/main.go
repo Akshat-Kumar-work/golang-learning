ad9e65ef0d4581baf9a44112e47b678ec6444b09
Akshat <iamakku0.0.0.1@gmail.com>
2025-11-10 21:37:24 +0530

    pointers

---
+++ 12_pointers/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+//pointers are the address of memory location of variables which we store
+
+func changeNumbyvalue(num int) {
+	num = 5
+	fmt.Println("in changenum by value", num)
+}
+
+func changeNumByReference(num *int) {
+	*num = 5
+	fmt.Println("in changenum by reference ", num)
+}
+
+func main() {
+
+	num := 1
+
+	fmt.Println("memory address is", &num)
+
+	//here we are passing by value
+	changeNumbyvalue(num)
+
+	fmt.Println("after change num by value", num)
+
+	changeNumByReference(&num)
+
+	fmt.Println("after change num by reference", num)
+
+}
