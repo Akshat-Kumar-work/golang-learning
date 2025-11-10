de73e613dd7156a2ccbdfd1298b7c097202ad15e
Akshat <iamakku0.0.0.1@gmail.com>
2025-11-10 21:17:21 +0530

    more func

--- functions/main.go
+++ functions/main.go
@@ -11,8 +11,18 @@ func getLangs() (string, string, string, int) {
 	return "gloang", "js", "python", 1
 }
 
+// callback func
 func process(fn func(a int, b int) int) {
 	fn(1, 2)
+}
+
+// variadic function : who accept multiple arguments
+func variadicFunc(nums ...int) int {
+	sum := 0
+	for _, value := range nums {
+		sum += value
+	}
+	return sum
 }
 
 func main() {
@@ -24,4 +34,7 @@ func main() {
 	fmt.Println(lang1, lang2, lang3, num)
 
 	process(sum)
+
+	multiplesum := variadicFunc(1, 2, 3, 4, 5, 6)
+	print(multiplesum)
 }
