3c3c2a377547524340abb8f8013b94daf064e79b
Akshat <iamakku0.0.0.1@gmail.com>
2025-11-12 11:05:31 +0530

    mutex

--- 20_mutex/main.go
+++ 20_mutex/main.go
@@ -13,13 +13,15 @@ type post struct {
 //Mutex stands for “mutual exclusion”.
 //It’s a synchronization primitive that ensures only one goroutine at a time can access a shared piece of data.
 //Basically it prevent from race conditions like multiple thread try to update the views parallely
-//so by using mutex we lock the views field so that only 1 thread can update the field at a time
+//so by using mutex we lock the views field so that only 1 thread/goRoutine can update the field at a time
 
 func (p *post) incrementViewOnPost(wg *sync.WaitGroup) {
-	defer wg.Done()
+	defer func() {
+		p.mu.Unlock()
+		wg.Done()
+	}()
 	p.mu.Lock()
 	p.views += 1
-	p.mu.Unlock()
 }
 
 func main() {
